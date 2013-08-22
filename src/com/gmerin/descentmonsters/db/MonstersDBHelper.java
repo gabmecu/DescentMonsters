29d5d7cd753ecd28c10573b95bfde6beed7749c6
Gabi <gabmecu@gmail.com>
2013-08-22 15:58:02 +0200

    Este commit cierra el bug [#24 Los monstruos no salen ordenados en inglés]
    Se ha modificado la BD añadiendo una nueva columna a la tabla
    monstruos_genericos para albergar el nombre del monstruo en inglés.
    Realmente bastaría con poner la inicial de cada monstruos ya que dicho
    valor no se usa (el nombre de cada monstruo se coge del xml).
    
    Aprovechando el commit, también se han añadido comentarios a la clase
    MonstersDBHelper.java

--- src/com/gmerin/descentmonsters/db/MonstersDBHelper.java
+++ src/com/gmerin/descentmonsters/db/MonstersDBHelper.java
@@ -1,5 +1,7 @@
 package com.gmerin.descentmonsters.db;
 
+import java.util.Locale;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -42,11 +44,11 @@ public class MonstersDBHelper {
 	
 	public static final String TOKEN_IDMONACT = ":id:";
 			
-	private static String monSQL = "SELECT MG.id AS _id, MG.nombre, MG.tipo_ataque, MG.tam_peana, E.cod, R1.id, R2.id "+
+	private static String monSQL = "SELECT MG.id AS _id, MG.nombre_en, MG.tipo_ataque, MG.tam_peana, E.cod, R1.id, R2.id "+
 								   "FROM Expansiones AS E, Monstruos_Genericos AS MG, Rasgos AS R1, Rasgos AS R2 "+
 								   "WHERE E.cod=MG.cod_expansion AND MG.rasgo1=R1.id AND MG.rasgo2=R2.id ";							   
 	
-	private static String actXSQL = "SELECT MG.id AS _id, MG.nombre, MG.tipo_ataque, MG.tam_peana, E.cod, R1.id, R2.id, "+ 
+	private static String actXSQL = "SELECT MG.id AS _id, MG.nombre_en, MG.tipo_ataque, MG.tam_peana, E.cod, R1.id, R2.id, "+ 
 									"MA.lider, MA.vida, MA.velocidad, MA.defensa, MA.ataque, MA.grupo2h, MA.grupo3h, MA.grupo4h, MA.acto, MA.id "+
 									"FROM Expansiones AS E, Monstruos_Genericos AS MG, Rasgos AS R1, Rasgos AS R2, Monstruos_Acto AS MA "+
 									"WHERE E.cod=MG.cod_expansion AND MG.rasgo1=R1.id AND MG.rasgo2=R2.id "+
@@ -60,8 +62,6 @@ public class MonstersDBHelper {
 									 "FROM Capacidades_Monstruos AS CM, Capacidades AS C "+
 									 "WHERE CM.id_cap=C.id AND CM.id_mon_act="+TOKEN_IDMONACT+";";
 	
-	private static String orderSQL = "ORDER BY MG.nombre;";
-	
 	private static final String DB_NAME = "monsters";
 	private static final int DB_VERSION = 1;
 	
@@ -72,25 +72,49 @@ public class MonstersDBHelper {
 		dbHelper = new DBOpenHelper(context);
 		db = dbHelper.getReadableDatabase();
 	}
-	
+	/**
+	 * Devuelve un cursor con la información general de los monstruos.
+	 * Filtra los elementos que no cumplan las preferencias del usuario (filtros + configuración)
+	 * @param prefHelp
+	 * @return
+	 */
 	public Cursor getMonsters(PreferencesHelper prefHelp) {
 		// Modificamos la consulta en función de los filtros y la configuración
 		String auxSQL = filterSQL.replace(TOKEN_ATTACKS, prefHelp.getAttackTypesToFilter());
 		auxSQL = auxSQL.replace(TOKEN_EXPANSIONS, prefHelp.getExpansionsToFilter());
 		auxSQL = auxSQL.replace(TOKEN_TRAITS, prefHelp.getTraitsToFilter());
 		// Construimos la consulta final
-		String fullSQL = monSQL+auxSQL+orderSQL;
+		String fullSQL = monSQL+auxSQL+getOrderSQL();
 		return db.rawQuery(fullSQL, null);
 	}
 	
+	/**
+	 * Devuelve un cursor con las especificaciones de los monstruos del acto 1. 
+	 * Filtra los elementos que no cumplan las preferencias del usuario (filtros + configuración).
+	 * @param prefHelp
+	 * @return
+	 */
 	public Cursor getAct1Monsters(PreferencesHelper prefHelp) {
 		return getActXMonsters(prefHelp, 1);
 	}
 	
+	/**
+	 * Devuelve un cursor con las especificaciones de los monstruos del acto 2. 
+	 * Filtra los elementos que no cumplan las preferencias del usuario (filtros + configuración).
+	 * @param prefHelp
+	 * @return
+	 */
 	public Cursor getAct2Monsters(PreferencesHelper prefHelp) {
 		return getActXMonsters(prefHelp, 2);
 	}
 	
+	/**
+	 * Devuelve un cursor con las especificaciones de los monstruos del acto pasado como parámetro.
+	 * Filtra los elementos que no cumplan las preferencias del usuario (filtros + configuración).
+	 * @param prefHelp
+	 * @param act
+	 * @return
+	 */
 	private Cursor getActXMonsters(PreferencesHelper prefHelp, int act) {
 		// Modificamos la consulta en función de los filtros y la configuración
 		String auxSQL = filterSQL.replace(TOKEN_ATTACKS, prefHelp.getAttackTypesToFilter());
@@ -99,15 +123,37 @@ public class MonstersDBHelper {
 		// Nos quedamos con los mosntruos del acto correxpondiente
 		String mainSQL = actXSQL.replace(TOKEN_ACT, Integer.toString(act));
 		// Construimos la consulta final
-		String fullSQL = mainSQL+auxSQL+orderSQL;
+		String fullSQL = mainSQL+auxSQL+getOrderSQL();
 		return db.rawQuery(fullSQL, null);
 	}
 	
+	/**
+	 * Dado el identificador de un monstruo de un determinado acto, devuelve un cursor con todas sus habilidades.
+	 * @param id_mon_act
+	 * @return
+	 */
 	public Cursor getMonActAbilities(int id_mon_act) {
 		String fullSQL = monAbSQL.replace(TOKEN_IDMONACT, Integer.toString(id_mon_act));
 		return db.rawQuery(fullSQL, null);
 	}
 	
+	/**
+	 * Indica la sentencia de ordenación en función del idioma del sistema.
+	 * @return
+	 */
+	private String getOrderSQL() {
+		if (Locale.getDefault().getLanguage().startsWith("es"))
+			return "ORDER BY MG.nombre_es;";
+		else
+			return "ORDER BY MG.nombre_en;";
+	}
+	
+	/**
+	 * Clase que permite realizar la conexión a la base de datos. Además, si la BD no se encuentra instalada,
+	 * la instala en el sistema.
+	 * @author Gabi
+	 *
+	 */
 	private class DBOpenHelper extends SQLiteAssetHelper {
 
 		public DBOpenHelper(Context context) {
